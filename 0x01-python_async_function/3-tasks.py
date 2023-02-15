229d38dcf459f6245709c3c9c87947646cda97c8
holabayor <liasu.olabayo@gmail.com>
2023-02-15 08:27:40 +0100

    tasks 3

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" multiple coroutines at the same time with async """
+import asyncio
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    return asyncio.create_task(wait_random(max_delay))
